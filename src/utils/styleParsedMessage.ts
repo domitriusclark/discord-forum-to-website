744bfe48c535e6f6583d5434640009de3eb714bc
Domitrius Clark <domitriusaclark@gmail.com>
2022-12-13 02:29:24 -0500

    Working on some base styles for code blocks.

--- src/utils/styleParsedMessage.ts
+++ src/utils/styleParsedMessage.ts
@@ -4,5 +4,16 @@ export default function styleParsedMessage(content: HTMLElement): HTMLElement {
     a.setAttribute("class", "hover:underline text-blue-500");
   });
 
+  // add some base styling for a code block
+  content.querySelectorAll("pre").forEach((pre) => {
+    pre.setAttribute("class", "bg-gray-800 p-4 text-purple-400 rounded-md");
+  });
+
+  // add some base styling for a code block
+  content.querySelectorAll("code").forEach((code) => {
+    // style the code block for a dark mode with purples
+    code.setAttribute("class", "bg-gray-800 text-red-400 p-2 rounded-md");
+  });
+
   return content;
 }
